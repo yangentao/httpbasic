951ef5f42ed97d32887783a60d8ef81ee886685d
Entao <entaoyang@163.com>
2025-04-30 20:40:02 +0800

    1.0.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,7 @@ plugins {
 
 group = "io.github.yangentao"
 
-version = "1.0.3"
+version = "1.0.4"
 val artifactName = "httpbasic"
 val githubLib = "httpbasic"
 val descLib = "Http basic defines, Kotlin."
--- src/main/kotlin/io/github/yangentao/httpbasic/HttpBasic.kt
+++ src/main/kotlin/io/github/yangentao/httpbasic/HttpBasic.kt
@@ -2,7 +2,29 @@
 
 package io.github.yangentao.httpbasic
 
+import java.io.File
 import java.util.*
+
+/**
+ * Multipart form data, file upload
+ */
+open class HttpFileParam(val name: String, val httpFile: HttpFile) {
+    val filename: String get() = httpFile.filename
+    val file: File get() = httpFile.file
+    val mime: String get() = httpFile.mime
+
+    override fun toString(): String {
+        return "HttpFileParam(name=$name, filename=$filename, length:${file.length()}, file:${file.canonicalPath}, mime:$mime)"
+    }
+}
+
+open class HttpFile(val file: File, val filename: String = file.name, val mime: String = Mimes.ofFile(filename)) {
+    override fun toString(): String {
+        return "HttpFile(file:${file.canonicalPath}, filename=$filename, mime:$mime)"
+    }
+}
+
+fun File.httpFile(filename: String? = null, mime: String? = null) = HttpFile(this, filename ?: this.name, mime ?: Mimes.ofFile(filename ?: this.name))
 
 /**
  * Ignore case map, http headers
