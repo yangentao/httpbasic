ef7f9bbb923b72b08b02a24bac2aa4bb57749e02
Entao <entaoyang@163.com>
2025-04-30 19:59:09 +0800

    1.0.3

--- src/main/kotlin/io/github/yangentao/httpbasic/HttpFileParam.kt
+++ src/main/kotlin/io/github/yangentao/httpbasic/HttpFileParam.kt
@@ -7,8 +7,18 @@ import java.io.File
 /**
  * Multipart form data, file upload
  */
-open class HttpFileParam(val name: String, val filename: String, val file: File, val mime: String = Mimes.ofFile(filename)) {
+open class HttpFileParam(val name: String, val httpFile: HttpFile) {
+    val filename: String get() = httpFile.filename
+    val file: File get() = httpFile.file
+    val mime: String get() = httpFile.mime
+
     override fun toString(): String {
         return "HttpFileParam(name=$name, filename=$filename, length:${file.length()}, file:${file.canonicalPath}, mime:$mime)"
+    }
+}
+
+open class HttpFile(val file: File, val filename: String = file.name, val mime: String = Mimes.ofFile(filename)) {
+    override fun toString(): String {
+        return "HttpFile(file:${file.canonicalPath}, filename=$filename, mime:$mime)"
     }
 }
